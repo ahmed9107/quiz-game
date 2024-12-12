194d634b666578cc92af4298d3e61fc00be0704f
unknown <ahmedlemarshall@gmail.com>
2024-12-12 10:43:42 -0500

    ...

--- script/script.js
+++ script/script.js
@@ -2,7 +2,8 @@ function getQuestions() {
   let request = new XMLHttpRequest();
   request.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      console.log(this.responseText);
+      let questions = JSON.parse(this.responseText);
+      console.log(questions[0].title);
     }
   }
   request.open("GET", "../json/questions.json", true);
