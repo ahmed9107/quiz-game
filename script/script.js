57f5f48511295a4841753b6db75807884c8f1619
unknown <ahmedlemarshall@gmail.com>
2024-12-17 22:52:20 -0500

    ...

--- script/script.js
+++ script/script.js
@@ -1,16 +1,24 @@
 // Select Elements:
-let questCount = document.querySelector(".quiz-info .count");
+let countSpan = document.querySelector(".quiz-info .count");
 
 function getQuestions() {
   let request = new XMLHttpRequest();
   request.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      let questions = JSON.parse(this.responseText);
+      let questObj = JSON.parse(this.responseText);
+      let questCount = questObj.length;
+      console.log(questCount);
+
+      // Create bullets + Set question count
+      createBullets(questCount);
     }
-  }
+  };
   request.open("GET", "../json/questions.json", true);
   request.send();
 }
 
+getQuestions();
+
 function createBullets(num) {
+  countSpan.innerHTML = num;
 }
