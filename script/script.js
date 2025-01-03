f7ed8e81898a4a241b3a3664859460d7f05a0332
unknown <ahmedlemarshall@gmail.com>
2025-01-03 10:56:32 -0500

    ...

--- script/script.js
+++ script/script.js
@@ -43,7 +43,7 @@ function addQuestions(obj, count) {
   let questionTxt   = document.createTextNode(obj.title);
   questionTitle.appendChild(questionTxt);
   quizArea.appendChild(questionTitle);
-  for (let i = 0; i <= 4; i++) {
+  for (let i = 1; i <= 4; i++) {
     let mainDiv       = document.createElement("div");
     mainDiv.className = "answer";
     let radioInput    = document.createElement("input");
@@ -51,6 +51,11 @@ function addQuestions(obj, count) {
     radioInput.type   = "radio";
     radioInput.id     = `answer_${i}`;
     radioInput.dataset.answer = obj[`answer_${i}`];
+
+    // First Option selected by default:
+    if (i === 1) {
+      radioInput.checked = true;
+    }
 
     let label     = document.createElement("label");
     label.htmlFor = `answer_${i}`;
